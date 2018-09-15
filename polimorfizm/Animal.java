c4debdfc6bd8ad6b3c7af034750c20a8090b98f1
Szymon Rymarski <szymonrymarski@gmail.com>
2018-09-15 08:00:38 +0200

    tutorials

---
+++ polimorfizm/Animal.java
@@ -0,0 +1,22 @@
+package polimorfizm;
+
+public class Animal {
+
+    private String name;
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name=name;
+    }
+
+    public Animal(String name){
+        setName(name);
+    }
+
+    public void getSound(){
+        System.out.println("Jestem zwierzęciem i nazywam się "+getName());
+    }
+}
